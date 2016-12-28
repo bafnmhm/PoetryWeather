d379eb9ea7129739a2a527a4d91e94cfb05b089d
Zf.Chen <bafnmhm@foxmail.com>
2016-12-28 23:25:14 +0800

    add gird and label

--- src/main.py
+++ src/main.py
@@ -6,15 +6,43 @@ class Window(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
+        # set base window
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.setFixedSize(400, 700)
 
+        # shadow
         shadow = QtGui.QGraphicsDropShadowEffect(self)
         shadow.setBlurRadius(26)
         shadow.setColor(QtGui.QColor(100, 100, 100))
         shadow.setOffset(0, 0)
         self.setGraphicsEffect(shadow)
+
+        # create gird and day label
+        labelDay1 = QtGui.QLabel("<p style='color: #666;font-family: arial'>Sunday</p>")
+        labelDay2 = QtGui.QLabel("<p style='color: #666;font-family: arial'>Monday</p>")
+        labelDay3 = QtGui.QLabel("<p style='color: #666;font-family: arial'>Tuesday</p>")
+
+        labelTemp1 = QtGui.QLabel("<p style='color: #666;font-family: arial'>20C-21C</p>")
+        labelTemp2 = QtGui.QLabel("<p style='color: #666;font-family: arial'>20C-21C</p>")
+        labelTemp3 = QtGui.QLabel("<p style='color: #666;font-family: arial'>20C-21C</p>")
+
+        vBox = QtGui.QVBoxLayout()
+        vBox.setAlignment(QtCore.Qt.AlignCenter)
+        vBox.addStretch(8)
+
+        girdLayout = QtGui.QGridLayout()
+        girdLayout.addItem(QtGui.QSpacerItem(180, 1), 0, 2)
+        girdLayout.addWidget(labelDay1, 0, 1)
+        girdLayout.addWidget(labelDay2, 1, 1)
+        girdLayout.addWidget(labelDay3, 2, 1)
+        girdLayout.addWidget(labelTemp1, 0, 3)
+        girdLayout.addWidget(labelTemp2, 1, 3)
+        girdLayout.addWidget(labelTemp3, 2, 3)
+        vBox.addLayout(girdLayout)
+        vBox.addStretch(1)
+
+        self.setLayout(vBox)
+        self.resize(400, 700)
 
     def paintEvent(self, event):
         borderRadius = 15
